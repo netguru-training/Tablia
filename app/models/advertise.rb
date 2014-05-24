4a08d962e1fde0d2c43e71854ef122a93ca2aa6e
saasbook <saasbook@saasbook.(none)>
2014-05-24 06:55:10 -0300

    add indexes and scopes to advertises

--- app/models/advertise.rb
+++ app/models/advertise.rb
@@ -6,4 +6,16 @@ class Advertise
   field :body, type: String
 
   belongs_to  :user
+
+  index({user: 1}, { unique: true, name: "user_index" }
+  index({created_at: 1}, { unique: false, name: "created_at" }
+
+  scope :recent,(mojazmienna) -> {
+    where(created_at: 3.days.ago..Time.now)
+  }
+
+  scope :by_user,(user_name) -> {
+    where(user: user_name)
+  }
+
 end
