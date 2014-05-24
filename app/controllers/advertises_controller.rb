30cb2445161a32433d88b2148246d01f41c2704d
HarenBroog <damian.romanow@gmail.com>
2014-05-24 11:13:55 +0200

    Created AdvertiseController

---
+++ app/controllers/advertises_controller.rb
@@ -0,0 +1,20 @@
+class AdvertiseController < ApplicationController
+  expose(:advertises) { Advertise.all }
+  expose(:advertise, attributes: :advertise_params)
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  private
+
+  def advertise_params
+    params.require(:advertise).permit(:title, :body )
+  end
+
+end
