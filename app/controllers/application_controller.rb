bf80368224942d5661907f13472c9dc964b083cd
jniechcial <jakub.niechcial@gmail.com>
2014-05-24 12:46:34 +0200

    Added first name and last name to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, except: [:home]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
@@ -12,4 +13,11 @@ class ApplicationController < ActionController::Base
   def home
     
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :first_name
+      devise_parameter_sanitizer.for(:sign_up) << :last_name
+    end
 end
