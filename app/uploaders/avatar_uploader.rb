090328aabfb31e9cabaa644ab7a4a61849f979c1
blazejhadzik <blazej.hadzik@netguru.pl>
2014-05-25 16:22:39 +0200

    heroku carierwave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,10 +15,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     ActionController::Base.helpers.asset_path("fallback/default_avatar.jpeg")
   end
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   def extension_white_list
     %w(jpg jpeg gif png)
   end
--- app/uploaders/product_uploader.rb
+++ app/uploaders/product_uploader.rb
@@ -15,10 +15,6 @@ class ProductUploader < CarrierWave::Uploader::Base
     ActionController::Base.helpers.asset_path("fallback/default_image.jpg")
   end
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
   def extension_white_list
     %w(jpg jpeg gif png)
   end
