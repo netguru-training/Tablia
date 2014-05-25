09743364743e4b79f97b643d6732c5e95c9c9431
jniechcial <jakub.niechcial@gmail.com>
2014-05-25 09:22:08 +0200

    Mailer controller

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,3 +1,10 @@
 class MessageMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def send_message_to_user_about_ad(user, advertise)
+    @user = user
+    @email_with_name = "#{@user.name} <#{@user.email}>"
+    @advertise = advertise
+    mail(from: current_user.email, to: @email_with_name, subject: "User #{current_user.name} sent you a message about your #{advertise.title}!")
+  end
 end
