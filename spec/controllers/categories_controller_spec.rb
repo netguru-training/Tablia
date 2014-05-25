ec903d910307d6788cfd79210618e8f73b192504
jniechcial <jakub.niechcial@gmail.com>
2014-05-25 13:58:27 +0200

    Category further specs

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -7,4 +7,36 @@ describe CategoriesController do
 
   let!(:advertise_one) { create(:advertise, user: user, category: category_one) }
   let!(:advertise_two) { create(:advertise, user: user, category: category_two) }
+
+  context 'for signed in users' do
+    before do
+      request.env['warden'].stub authenticate!: user
+      controller.stub current_user: user
+    end
+  end
+
+  context 'for unsigned users' do
+    describe 'GET new' do
+      it 'redirects to sign_in' do
+        get :new
+        controller.should redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  context 'for both signed and unsigned users' do
+    describe 'GET index' do
+      it "exposes all categories" do
+        get :index
+        expect(controller.categories.count).to eq(2)
+      end
+    end
+
+    describe 'GET show' do
+      it "exposes exact category" do
+        get :show, id: category_one.id
+        expect(controller.category).to eq(category_one)
+      end
+    end
+  end
 end
