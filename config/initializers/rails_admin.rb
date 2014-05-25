feddee5fa82442f310f6a35cb77f44d465a4b08a
jniechcial <jakub.niechcial@gmail.com>
2014-05-25 14:36:10 +0200

    Rails admin fix for routing

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -11,7 +11,7 @@ RailsAdmin.config do |config|
   config.authorize_with do |controller|
     unless current_user.try(:admin?)
       flash[:error] = "You are not an admin"
-      redirect_to root_path
+      redirect_to main_app.root_path
     end
   end
 
